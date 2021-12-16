8501c091c5b308afd3f797b915c23ba66ca050f1
fabien <fabien.andrian@gmail.com>
2021-12-16 13:05:06 +0100

    hamburger button as component

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,30 @@
+import { useState } from "react"
+import { ButtonHamburger } from "./button-hamburger"
+
+
 export default function Navigation () {
+
+    const [menuIsActive, setMenuIsActive] = useState(false)
+
     return (
         <div className="navigation">
             <div className="col">
                 <div> Fabien A. </div>
             </div>
             <div className="col">
-                <button> Skills </button>
-                <button> Projects </button>
-                <button> Contact me </button>
-                <button> Dark </button>
+                <div className={'menu' + (menuIsActive ? " menu-active" : "")}>
+                    <button> Skills </button>
+                    <button> Projects </button>
+                    <button> Contact me </button>
+                    <button> Dark </button>
+                </div>
             </div>
+            <ButtonHamburger 
+                menuIsActive={menuIsActive}
+                setMenuIsActive={setMenuIsActive}
+                minWidth={'601px'}
+            />
+           
         </div>
     )
 }

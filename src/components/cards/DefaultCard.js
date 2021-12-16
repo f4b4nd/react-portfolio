fe617f737fee69c865522e8bb1f0e8fb6c8667e8
fabien <fabien.andrian@gmail.com>
2021-12-16 15:22:08 +0100

    defaultcard as compound component

---
+++ src/components/cards/DefaultCard.js
@@ -0,0 +1,100 @@
+import styled from 'styled-components'
+
+
+export const DefaultCard = ({children, ...restProps}) => (
+    <StyledDefaultCard className="card" {...restProps} >
+        {children}
+    </StyledDefaultCard>
+)
+
+
+DefaultCard.Image = ({...restProps}) => (
+    <div className="card-image">
+        <img {...restProps} />
+    </div>
+)
+
+DefaultCard.Title = ({children, ...restProps}) => (
+    <div className="card-title" {...restProps}>
+        {children}
+    </div>
+)
+
+DefaultCard.Description = ({children, ...restProps}) => (
+    <div className="card-description" {...restProps}>
+        {children}
+    </div>
+)
+
+DefaultCard.Button = ({children, ...restProps}) => (
+    <div className="card-button" >
+        <a {...restProps}>
+            {children}
+        </a>
+    </div>
+)
+
+
+const StyledDefaultCard = styled.div`
+
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1em;
+
+    border: 1px solid hsla(0, 0%, 82.7%, .397);
+    border-radius: 10px;
+
+    &:hover {
+        box-shadow: 0 20px 30px -10px rgba(0, 0, 0, 0.2);
+    }
+
+    .card-image {
+        height: 250px;
+        max-width: 90%;
+        display: flex;
+        justify-content: center;
+        justify-self: flex-start;
+
+        img {
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+            margin: auto;
+        }
+    }
+
+    & > *:not(:first-of-type):not(:last-of-type){
+        margin: 5px 0;
+    }
+
+    .card-title {
+        color: #666;
+        font-size: 22px;
+        line-height: 24px;
+        font-weight: 700;
+    }
+
+    .card-description {
+        color: #666;
+        font-size: 17px;
+        line-height: 1.5rem;
+    }
+
+    .card-button {
+        background-color: #06b6d4;
+        color: white;
+        border-radius: 5px;
+        padding: 0.5em;
+
+        a:visited {
+            color: white;
+        }
+
+        a {
+            text-decoration: none;
+        }
+    }
+
+`
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,4 @@
-import { ProjectCard } from "./cards/ProjectCard"
+import { DefaultCard } from "./cards/DefaultCard"
 
 
 export default function Projects () {
@@ -7,49 +7,56 @@ export default function Projects () {
         {
             title: 'Netflix', 
             description : 'Build a Netflix Clone, with React, Redux, Stripe, SCSS, firebase.',
-            image : 'link',
+            image : '/images/react-netflix.png',
+            projectUrl: 'https://react-netflix-clone-f4b4nd.netlify.app/'
         },
         {
             title: 'Airbnb', 
             description : 'Build a Airbnb Clone, with React, Redux, Stripe, SCSS, firebase.',
             image : 'link',
+            projectUrl: ''
         },
         {
             title: 'Netflix', 
             description : 'Build a Netflix Clone, with React, Redux, Stripe, SCSS, firebase.',
             image : 'link',
+            projectUrl: ''
         },
         {
             title: 'Airbnb', 
             description : 'Build a Airbnb Clone, with React, Redux, Stripe, SCSS, firebase.',
             image : 'link',
+            projectUrl: ''
         },
         {
             title: 'Netflix', 
             description : 'Build a Netflix Clone, with React, Redux, Stripe, SCSS, firebase.',
             image : 'link',
+            projectUrl: ''
         },
         {
             title: 'Airbnb', 
             description : 'Build a Airbnb Clone, with React, Redux, Stripe, SCSS, firebase.',
             image : 'link',
+            projectUrl: ''
         },
     ]
+ 
 
     const ProjectsCards = projects.map((item, idx) => (
-        <ProjectCard 
-            key={idx} 
-            title={item.title}
-            description={item.description}
-            image={item.image}
-        />
+        <DefaultCard  key={idx} >
+            <DefaultCard.Image src={item.image} />
+            <DefaultCard.Title>{item.title}</DefaultCard.Title>
+            <DefaultCard.Description>{item.description}</DefaultCard.Description>
+            <DefaultCard.Button href={item.projectUrl}>Voir le projet</DefaultCard.Button>
+        </DefaultCard>
     ))
 
     return (
         <div className="projects">
             <h1> Projects </h1>
             <h2> Achievements, Some Cool Stuff that I have worked on !</h2>
-            <div className="grid"> 
+            <div className="projects-cards"> 
                 {ProjectsCards} 
             </div>
         </div>
